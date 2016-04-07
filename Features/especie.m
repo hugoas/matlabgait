e630bfe8a05ff7370a115d9f1e452721fdcd6b8c
Hugo Souza <hugosouz@hotmail.com>
2016-04-06 22:52:26 -0300

    Corrigindo
    Limpando arquivos inúteis.

--- Features/especie.m
+++
@@ -1,9 +0,0 @@
-x = importdata('dataTest1.txt') 
-dataX = [x(:,1), x(:,2)];
-%dataY = [x(:,3), x(:,4)];
-
-groups = ismember(dataset,'1.1');
-[train, test] = crossvalind('holdOut',groups);
-cp = classperf(groups);
-% 
-svmStruct = svmtrain(dataX(train,:),groups(train),'showplot',true);
